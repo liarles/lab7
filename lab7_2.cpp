94be9fbf4d97866ebb94105503490767a563edc0
liarles <62631264+liarles@users.noreply.github.com>
2024-12-03 15:31:46 +0700

    Update lab7_2.cpp

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,18 +1,47 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main() 
+{
+    string name;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: ";
+    cin >> name;
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
+    
+    int number;
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << name << ":";
+    cin >> number;
+    int numDigits = log10(number)+1;
+    int firstTwoDigits = number/pow(10, numDigits-2);
+    int result = firstTwoDigits -12;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+    cout << "Fahsai: I think you may be GEAR " << result << ". I have a free movie ticket for you.\n";
+   
+    string movie;
+    cout << "Fahsai: Let's go to the cinema together!!!\nFahsai: What movie do you want to watch?\n";
+    cout << name << ":";
+    cin >> movie;
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    
+    string day;
+    cout << name << ":";
+    cin >> day;
+    
+    cout << "Fahsai: " << day <<"....that is OK!!! I'm looking forward to watching " << movie << " with you.\n";
+    string whatever;
+    cout << name << ":";
+    cin >>whatever;
+    
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \(^ ^)/";
+    
+    
+
+    
+    
+
+
+    return 0;
+}
